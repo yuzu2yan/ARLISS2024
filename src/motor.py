f2c350fbcb3b8833723a28e11c4d5e7d35d3c1d3
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-06-30 14:40:53 +0900

    back

--- src/motor.py
+++ src/motor.py
@@ -90,7 +90,6 @@ if __name__ == '__main__':
         if c in movement.keys():
             movement[c]()
         elif c == 'z':
-            drive.GPIO.cleanup()
             break
         else:
             print('Invalid input')
