c62ec965a3455c208649e192e6f68172526d059c
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-06-30 17:59:51 +0900

    back to pigpio

--- src/motor.py
+++ src/motor.py
@@ -1,52 +1,34 @@
 import pigpio
 import time
-import RPi.GPIO as GPIO
 
 # pigpio library : https://abyz.me.uk/rpi/pigpio/python.html
 FRONT = [27, 9] # Left, Right
 REAR = [17, 10] # Left, Right
 SEPA_FIN = 13
 SEPA_RIN = 19
-PINS = [FRONT[0], FRONT[1], REAR[0], REAR[1], SEPA_FIN, SEPA_RIN]
+PINS = FRONT + REAR + [SEPA_FIN, SEPA_RIN]
 class Motor(object):
     def __init__(self):
-        # Motor.pi = pigpio.pi()
+        Motor.pi = pigpio.pi()
         self.max_dutyrate = 1.0
-        GPIO.setmode(GPIO.BCM)
         for pin in PINS:
-            # Motor.pi.set_mode(pin, pigpio.OUTPUT)
-            # Motor.pi.set_PWM_frequency(pin, 10000)
-            # Motor.pi.set_PWM_range(pin, 100)
-            GPIO.setup(pin, GPIO.OUT)
+            Motor.pi.set_mode(pin, pigpio.OUTPUT)
+            Motor.pi.set_PWM_frequency(pin, 10000)
+            Motor.pi.set_PWM_range(pin, 100)
             
     
     def forward(self):
-        # Motor.pi.set_PWM_dutycycle(FRONT[0], 90) # Left
-        # Motor.pi.set_PWM_dutycycle(FRONT[1], 100) # Right
-        # [Motor.pi.set_PWM_dutycycle(pin, 0) for pin in REAR]
-        GPIO.output(FRONT[0], True)
-        GPIO.output(FRONT[1], True)
-        GPIO.output(REAR[0], False)
-        GPIO.output(REAR[1], False)
+        [Motor.pi.set_PWM_dutycycle(pin, 100) for pin in FRONT]
+        [Motor.pi.set_PWM_dutycycle(pin, 0) for pin in REAR]
         print("forward")
         
     def back(self):
-        # [Motor.pi.set_PWM_dutycycle(pin, 0) for pin in FRONT]
-        # [Motor.pi.set_PWM_dutycycle(pin, 100) for pin in REAR]
-        GPIO.output(REAR[0], True)
-        GPIO.output(REAR[1], True)     
-        GPIO.output(FRONT[0], False)
-        GPIO.output(FRONT[1], False)   
+        [Motor.pi.set_PWM_dutycycle(pin, 0) for pin in FRONT]
+        [Motor.pi.set_PWM_dutycycle(pin, 100) for pin in REAR]
         print("back")
     
     def stop(self):
-        # [Motor.pi.set_PWM_dutycycle(pin, 0) for pin in PINS]
-        GPIO.output(FRONT[0], False)
-        GPIO.output(FRONT[1], False)
-        GPIO.output(REAR[0], False)
-        GPIO.output(REAR[1], False)
-        GPIO.output(SEPA_FIN, False)
-        GPIO.output(SEPA_RIN, False)
+        [Motor.pi.set_PWM_dutycycle(pin, 0) for pin in PINS]
         print("stop")
         
     def turn_right(self):
@@ -67,20 +49,16 @@ class Motor(object):
         # time.sleep(3)
         # self.stop()
         # print("parachute separated")
-        # Motor.pi.write(SEPA_FIN, 1)
-        # Motor.pi.write(SEPA_RIN, 0)
-        GPIO.output(SEPA_FIN, True)
-        GPIO.output(SEPA_RIN, False)
+        Motor.pi.write(SEPA_FIN, 1)
+        Motor.pi.write(SEPA_RIN, 0)
         
                 
     def attach_para(self):
         # Motor.pi.set_PWM_dutycycle(SEPA_FIN, 0) 
         # Motor.pi.set_PWM_dutycycle(SEPA_RIN, 50) 
         # print("parachute attached")
-        # Motor.pi.write(SEPA_FIN, 0)
-        # Motor.pi.write(SEPA_RIN, 1)
-        GPIO.output(SEPA_FIN, False)
-        GPIO.output(SEPA_RIN, True)
+        Motor.pi.write(SEPA_FIN, 0)
+        Motor.pi.write(SEPA_RIN, 1)
 
 if __name__ == '__main__':
     drive = Motor()
