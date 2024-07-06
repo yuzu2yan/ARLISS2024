be4d8defcd7a9ee502caef32347879cdb75c3ff0
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-07-06 12:20:55 +0900

    yolov5

--- src/experiment/detect_cone.py
+++ src/experiment/detect_cone.py
@@ -1,55 +1,51 @@
 import cv2
 import datetime
 from picamera2 import Picamera2
-from ultralytics import YOLO
+from yolov5 import YOLOv5
 
+# YOLOv5のnanoモデルを読み込む
+model = YOLOv5('./best.pt')
 
-if __name__ == "__main__":
-    model = YOLO('./best.pt')
-    print("Model loaded successfully.")
-    # Initialize Picamera2
-    picam2 = Picamera2()
-    config = picam2.create_preview_configuration()
-    picam2.configure(config)
-    picam2.start()
-    print("Picamera2 initialized successfully.")
+# Picamera2の初期化
+picam2 = Picamera2()
+config = picam2.create_preview_configuration()
+picam2.configure(config)
+picam2.start()
+
+def process_frame():
     # Get a frame from Picamera2
     frame = picam2.capture_array()
+
     if frame is not None:
-        print("Frame read successfully.")
-        # Convert the frame to RGB 
+        # Convert the frame to RGB
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
 
         results = model(frame_rgb)
-        print("Model inference completed.")
+
         # Annotate the frame with the results
-        annotated_frame = results[0].plot()
+        annotated_frame = results.render()[0]
         
-        result_object = results[0]
+        result_object = results.pandas().xyxy[0]
 
         # Get the bounding box positions
-        bounding_boxes = result_object.boxes.xyxy
+        bounding_boxes = result_object[['xmin', 'ymin', 'xmax', 'ymax']].values
 
         # Get the class IDs
-        class_ids = result_object.boxes.cls
+        class_ids = result_object['class'].values
+
         # Get the class names
-        class_names_dict = result_object.names
+        class_names_dict = results.names
 
         for box, class_id in zip(bounding_boxes, class_ids):
             class_name = class_names_dict[int(class_id)]
             print(f"Box coordinates: {box}, Object: {class_name}")
-        
-        # # Display the annotated frame
-        # cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Save the annotated frame
         now = datetime.datetime.now()
         filename = now.strftime('%Y%m%d %H:%M:%S') + ".jpg"
         cv2.imwrite(filename, annotated_frame)
-
-        # # Break the loop if 'q' is pressed
-        # if cv2.waitKey(1) & 0xFF == ord("q"):
-        #     break
     else:
         print("Error: Frame not read successfully.")
 
-    picam2.stop()
-    # cv2.destroyAllWindows()
+if __name__ == "__main__":
+    process_frame()
