42b8c4a803a03e088671388e88c7e72211da06b7
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-07-06 13:45:07 +0900

    modify

--- src/experiment/down_link.py
+++ src/experiment/down_link.py
@@ -19,10 +19,10 @@ def main():
             gps = gnss.read_GPSData()
             if gps == [0,0]:
                 print("Waiting for GPS reception")
-                time.sleep(5)
+                # time.sleep(5)
             with open(filename, 'a') as f:
                 writer = csv.writer(f)
-                writer.writerow([now.strftime(gps[0], gps[1])])
+                writer.writerow([gps[0], gps[1]])
             pi.i2c_write_device(handle, gps)
             time.sleep(1)
     except KeyboardInterrupt:
