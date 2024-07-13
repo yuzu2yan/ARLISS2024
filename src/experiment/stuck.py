94a382a8b4365586dc46f47428f17cb2c51264bc
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-07-13 17:29:05 +0900

    print distance

--- src/experiment/stuck.py
+++ src/experiment/stuck.py
@@ -13,6 +13,7 @@ def stuck():
         time.sleep(5)
         gps = gnss.read_GPSData()
         distance = ground.cal_distance(pre_gps[0], pre_gps[1], gps[0], gps[1])
+        print("distance :", distance)
         if distance < 0.1:
             drive.stuck()
             drive.stop()
