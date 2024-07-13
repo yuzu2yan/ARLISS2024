44d60de64a77e16d8011fdbd86c06b61f3204a5c
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-07-13 17:26:58 +0900

    drive

--- src/experiment/stuck.py
+++ src/experiment/stuck.py
@@ -6,15 +6,16 @@ import ground
 import time
 
 def stuck():
+    drive = motor.Motor()
     while True:
-        motor.forward()
+        drive.forward()
         pre_gps = gnss.read_GPSData()
         time.sleep(5)
         gps = gnss.read_GPSData()
         distance = ground.cal_distance(pre_gps[0], pre_gps[1], gps[0], gps[1])
         if distance < 0.1:
-            motor.stuck()
-            motor.stop()
+            drive.stuck()
+            drive.stop()
             return
 
 
