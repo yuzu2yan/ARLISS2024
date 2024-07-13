f5772b26eb176fa558c6b1f7c252a8a71af59716
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-07-13 17:01:48 +0900

    motor

--- src/motor.py
+++ src/motor.py
@@ -2,8 +2,8 @@ import pigpio
 import time
 
 # pigpio library : https://abyz.me.uk/rpi/pigpio/python.html
-FRONT = [17, 13] # Left, Right
-REAR = [27, 19] # Left, Right
+FRONT = [27, 19] # Left, Right
+REAR = [17, 13] # Left, Right
 SEPA_FIN = 10
 SEPA_RIN = 9
 PINS = FRONT + REAR + [SEPA_FIN, SEPA_RIN]
