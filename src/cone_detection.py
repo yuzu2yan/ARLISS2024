902f86670f4a3706157c0f282a96ebece9523df9
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-08-02 14:24:46 +0900

    print

--- src/cone_detection.py
+++ src/cone_detection.py
@@ -22,7 +22,7 @@ def detect_cone(picam2, model, directory_path="./"):
         img_yuv[:,:,0] = clahe.apply(img_yuv[:,:,0]) # Apply CLAHE to the Y-channel (luminance)
         img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2RGB) # YUV => RGB
         print("CLAHE applied.")
-        cv2.imwrite(directory_path + '/' + now.strftime('%Y%m%d %H:%M:%S') + "_clahe.jpg", img)
+        # cv2.imwrite(directory_path + '/' + now.strftime('%Y%m%d %H:%M:%S') + "_clahe.jpg", img)
         results = model(img)
         print("Model inference completed.")
         # If nothing is detected, return 0
@@ -31,16 +31,19 @@ def detect_cone(picam2, model, directory_path="./"):
             return 0, 0, "not found", original_file_name, "not found"
         # Annotate the frame with the results
         annotated_frame = results[0].plot()
+        cv2.imwrite(directory_path + '/' + now.strftime('%Y%m%d %H:%M:%S') + "_annotated.jpg", annotated_frame)
         print("Frame annotated.")
         result_object = results[0]
         print("Results obtained.")
         try:
             # Get the bounding box positions
             bounding_boxes = result_object.boxes.xyxy
-            central_x = (bounding_boxes[0][0] + bounding_boxes[0][2]) / 2
+            central_x = (bounding_boxes[0][0] + bounding_boxes[0][2]) / 2.0
+            print("central_x:", central_x)
             percent = int(100 * result_object.boxes.conf[0])
             print("percent:", percent)
             red_cone_percent = (bounding_boxes[0][2] - bounding_boxes[0][0]) * (bounding_boxes[0][3] - bounding_boxes[0][1]) / (frame.shape[0] * frame.shape[1]) * 100
+            print("red_cone_percent:", red_cone_percent)
             print("Bounding boxes obtained.")
         except Exception as e:
             print("Error:", e)
