df44421319187664db42cf9ce25eb3ea0193d026
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-08-03 16:35:14 +0900

    turn 90

--- src/experiment/e2e_2.py
+++ src/experiment/e2e_2.py
@@ -139,14 +139,14 @@ def main():
                     phase = 2
                     break
                 pre_ang = ground.cal_heading_ang()[0]
-                while abs(pre_ang - ground.cal_heading_ang()[0]) > 45:
+                while abs(pre_ang - ground.cal_heading_ang()[0]) > 90:
                     drive.turn_right()
                     time.sleep(0.2)
             drive.forward()
             gps = gnss.read_GPSData()
             # var = ground.cal_distance(pre_gps[0], pre_gps[1], gps[0], gps[1])
             # if var < 1:
-            #     drive.stuck()
+            #     drive.stuck()\
             
     picam2.stop()
 
