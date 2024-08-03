5e38312486ce0848663577a39f64a4ad8bc3c345
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-08-03 16:48:32 +0900

    print

--- src/experiment/e2e_2.py
+++ src/experiment/e2e_2.py
@@ -141,6 +141,7 @@ def main():
                     break
                 pre_ang = ground.cal_heading_ang()[0]
                 while abs(pre_ang - ground.cal_heading_ang()[0]) > 45:
+                    print(abs(pre_ang - ground.cal_heading_ang()[0]))
                     drive.turn_right()
                     time.sleep(0.2)
             drive.forward()
