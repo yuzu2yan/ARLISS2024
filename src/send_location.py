38ee81c64193bd51ac19bd972913dac99fc61dae
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-08-05 11:41:39 +0900

    send location debug

--- src/send_location.py
+++ src/send_location.py
@@ -1,21 +1,15 @@
-import sys
-sys.path.append('./../')
 import gnss
 import pigpio
 import time
-import datetime
-import csv
-import struct
 
 def send_gps(gps):
     pi = pigpio.pi()
     handle = pi.i2c_open(1, 0x04)
     try:
-        while True:
-            pi.i2c_write_device(handle, str(gps[0]))
-            pi.i2c_write_device(handle, str(gps[1]))
-            time.sleep(1)
-    except KeyboardInterrupt:
+        pi.i2c_write_device(handle, str(gps[0]))
+        pi.i2c_write_device(handle, str(gps[1]))
+        time.sleep(1)
+    except Exception as e:
         pi.i2c_close(handle)
         pi.stop()        
     
