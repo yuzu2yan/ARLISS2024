95b97bd28abe91461a4f637f13e058cb1586fb80
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-08-25 15:00:34 +0900

    date

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
     
     Author : Yuzu
     Language : Python Ver.3.9.2
-    Last Update : 08/19/2024
+    Last Update : 08/25/2024
     Licence : MIT Licence
 """""""""""""""""""""""""""""""""""
 
